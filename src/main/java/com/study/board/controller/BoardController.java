971ed25ea4be59265859d3430e2f574dd2e65749
hyewon park <zip17430@icloud.com>
2024-10-07 05:57:02 +0900

    5주차 과제

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,4 +1,82 @@
 package com.study.board.controller;
 
+import com.study.board.dto.BoardDTO;
+import com.study.board.dto.BoardFileDTO;
+import com.study.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class BoardController {
+    //생성자 주입
+    private final BoardService boardService;
+
+   //주소
+    @GetMapping("/save")
+    //메서드이름
+    public String save() {
+        //return 화면 이름
+        return "save";
+    }
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) throws IOException {
+        System.out.println("boardDTO = " + boardDTO);
+        boardService.save(boardDTO);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        System.out.println("boardDTOList = " + boardDTOList);
+        return "list";
+    }
+
+    // /10, /1
+    @GetMapping("/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        // 조회수 처리
+        boardService.updateHits(id);
+        // 상세내용 가져옴
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        System.out.println("boardDTO = " + boardDTO);
+        if (boardDTO.getFileAttached() == 1) {
+            List<BoardFileDTO> boardFileDTOList = boardService.findFile(id);
+            model.addAttribute("boardFileList", boardFileDTOList);
+        }
+        return "detail";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable("id") Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update(BoardDTO boardDTO, Model model) {
+        boardService.update(boardDTO);
+        BoardDTO dto = boardService.findById(boardDTO.getId());
+        model.addAttribute("board", dto);
+        return "detail";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        boardService.delete(id);
+        return "redirect:/list";
+    }
+
 }
--- src/main/java/com/study/board/dto/BoardDTO.java
+++ src/main/java/com/study/board/dto/BoardDTO.java
@@ -1,4 +1,21 @@
 package com.study.board.dto;
 
-public class BoardDto {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BoardDTO {
+    //save와 이름 일치해야됨
+    private Long id; //글번호
+    private String boardWriter; //작성자
+    private String boardPass; //비밀번호
+    private String boardTitle; //제목
+    private String boardContents; //내용
+    private int boardHits; //조회수
+    private String createdAt; //작성시간
+
+
 }
